227437d7e6574dc902e3675e27e56cb2ab4734af
Orion Engleton <rsgrafx@gmail.com>
2019-12-03 22:49:43 +1300

    Set config to false

--- config/config.exs
+++ config/config.exs
@@ -4,4 +4,5 @@ config :wp_config_writer, :myxql,
   username: System.get_env("MYSQL_USER"),
   password: System.get_env("MYSQL_PASS"),
   hostname: System.get_env("MYSQL_HOST"),
-  port: System.get_env("MYSQL_PORT") || 3306
+  port: System.get_env("MYSQL_PORT") || 3306,
+  ssl: false

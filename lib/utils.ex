246c20c48eac8b2e12fd39fd27244da10adb9b10
Orion Engleton <rsgrafx@gmail.com>
2019-12-08 03:43:16 +1300

    Added Utils helpers

---
+++ lib/utils.ex
@@ -0,0 +1,19 @@
+defmodule WpConfigWriter.Utils do
+  def mask(string) when is_binary(string) do
+    do_mask(string, "")
+  end
+
+  def mask(_), do: :error
+
+  defp do_mask("", _), do: :error
+
+  defp do_mask(<<last::binary-size(1), ""::binary>>, string), do: string <> last
+
+  defp do_mask(<<first::binary-size(1), rest::binary>>, <<""::binary>> = string) do
+    do_mask(rest, string <> first)
+  end
+
+  defp do_mask(<<_::binary-size(1), rest::binary>>, string) do
+    do_mask(rest, string <> "*")
+  end
+end

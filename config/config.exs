fe16d04a0a63e5932046386042c20a8240360ee5
Orion Engleton <rsgrafx@gmail.com>
2019-12-08 15:27:01 +1300

    Add config setting for dns

--- config/config.exs
+++ config/config.exs
@@ -7,4 +7,7 @@ config :wp_config_writer, :myxql,
   port: System.get_env("MYSQL_PORT") || 3306,
   ssl: false
 
-config :wp_config_writer, :dns_provider, key: System.get_env("DIGITAL_OCEAN_API")
+config :wp_config_writer,
+       :dns_provider,
+       key: System.get_env("DIGITAL_OCEAN_API"),
+       ip_address: System.get_env("DNS_IP")
